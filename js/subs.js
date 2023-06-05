8c55be22fb1cc00129ddd1313a3062751acf3f6b
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-06-05 13:10:56 +0200

    add innerHtml to order page
    add live price and calories to build a sub page

--- js/subs.js
+++ js/subs.js
@@ -1,23 +1,137 @@
-hideElements();
+//Sauce Variables
+let sauceArr = [];
+let sauceCount = 0;
+//Topping Variables 
+let toppingArr = [];
+let toppingCount = 0;
+//order variables
+let orderArr = [];
+
+
+addTopping = () =>{
+    let toppingInp = document.getElementById("toppingSelect").value
+    toppingArr[toppingCount] = toppingInp;
+    toppingCount++;
+
+    console.log(toppingArr)
+}
+
+addSauce = () =>{
+    let sauceInp = document.getElementById("sauceSelect").value
+    sauceArr[sauceCount] = sauceInp;
+    sauceCount++; 
+
+    console.log(sauceArr);
+
+}
+
+
 
 makeSandwich = () =>{
     let name = document.getElementById("inputSubName").value;
     let bread = document.getElementById("breadSelect").value;
 
-    if(bread == "Choose a Bread"){
-        console.log("Choose another bread");
-    }
-    else{
-        console.log(name + bread)
-    }
 
-
+    
+       orderArr.push({
+            sandwichName: name,
+            sandwichBread: bread,
+            sandwichToppings: toppingArr,
+            sandwichSauce: sauceArr
+       });
+    
+    console.log(orderArr);
+    document.getElementById("sandwichForm").reset();
 }
 
 
-hideElements = () =>{
-    document.getElementById("collect").hidden = true;
-    console.log("hello")
-}
 
-document.getElementById("sandwichForm").reset();
+sandwichCost = () =>{
+    console.log("hello World")
+    let price = 0;
+    let calories = 0;
+    let bread = document.getElementById("breadSelect").value;
+    switch(bread) {
+        case "White":
+            price+= 15;
+            calories +=132;
+            break;
+        case "Ciabatta":
+            price+=20;
+            calories += 140;
+            break;
+        case "Rye":
+            price+=18;
+            calories += 110
+            break;
+        case "Sourdough":
+            price+=25;
+            calories += 185;
+            break;
+        case "French Loaf":
+            price+=30;
+            calories += 190;
+            break;
+        default:
+            calories = 0;
+            price = 0;
+      }
+
+      let toppingInp = document.getElementById("toppingSelect").value;
+      switch(toppingInp){
+            case "Chicken":
+                price += 10;
+                calories +=165;
+                break;
+            case "Beef":
+                price += 15;
+                calories +=250;
+                break;
+            case "Pulled Pork":
+                break;
+            case "Ham":
+                break;
+            case "Bacon":
+                break;
+            case "Tuna":
+                break;
+            case "Tomato":
+                break;
+            case "Pickles":
+                break;
+            case "Onion":
+                break;
+            case "Cucumber":
+                break;
+            case "Avocado":
+                break;
+            case "Lettuce":
+                break;
+            case "Coleslaw":
+                break;
+            case "Pineapple":
+                break;
+            case "Brie":
+                break;
+            case "Cheddar":
+                break;
+            case "Feta":
+                break;
+            case "Gouda":
+                break;
+            case "Cream Cheese":
+                break;
+            case "Goats Cheese":
+                break;
+            default:
+                price+=0;
+                calories+=0;
+      }
+  
+      console.log(toppingArr)
+
+      document.getElementById("orderCal").innerHTML = "Calories: "+calories
+      document.getElementById("orderPrice").innerHTML = "Price: R"+price+".00";
+
+      console.log(price);
+}
