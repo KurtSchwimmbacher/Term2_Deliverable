100a1b98257ed5e6bc854ea20697d309c3876c79
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-06-09 16:07:43 +0200

    add carousel js
    read carousel data out of a js file and put through innerHTML

---
+++ js/data.js
@@ -0,0 +1,74 @@
+let sandwichData = [
+    //let name = sandwichData[i].sandwichName;
+    //let bread = sandwichData[i].sandwichBread;
+    //let sauces = sandwichData[i].sandwichSauce;
+    //let toppings = sandwichData[i].sandwichToppings;
+    //let cost = sandwichData[i].sandwichCost;
+    //let calories = sandwichData[i].sandwichCal;
+
+    {
+        sandwichName : "Chicken Mayo",
+        sandwichBread : "Ciabatta",
+        sandwichSauce : ["Mayonnaise"],
+        sandwichTopping : ["Chicken","Pickles","Onion","Lettuce","Bacon"],
+        sandwichCost : 77,
+        sandwichCalories: 929
+    },
+    {
+        sandwichName : "Chicken Mayo",
+        sandwichBread : "Ciabatta",
+        sandwichSauce : ["Mayonnaise"],
+        sandwichTopping : ["Chicken","Pickles","Onion","Lettuce","Bacon"],
+        sandwichCost : 77,
+        sandwichCalories: 929
+    },
+    {
+        sandwichName : "Chicken Mayo",
+        sandwichBread : "Ciabatta",
+        sandwichSauce : ["Mayonnaise"],
+        sandwichTopping : ["Chicken","Pickles","Onion","Lettuce","Bacon"],
+        sandwichCost : 77,
+        sandwichCalories: 929
+    },
+    {
+        sandwichName : "Chicken Mayo",
+        sandwichBread : "Ciabatta",
+        sandwichSauce : ["Mayonnaise"],
+        sandwichTopping : ["Chicken","Pickles","Onion","Lettuce","Bacon"],
+        sandwichCost : 77,
+        sandwichCalories: 929
+    },
+    {
+        sandwichName : "Chicken Mayo",
+        sandwichBread : "Ciabatta",
+        sandwichSauce : ["Mayonnaise"],
+        sandwichTopping : ["Chicken","Pickles","Onion","Lettuce","Bacon"],
+        sandwichCost : 77,
+        sandwichCalories: 929
+    },
+    {
+        sandwichName : "Chicken Mayo",
+        sandwichBread : "Ciabatta",
+        sandwichSauce : ["Mayonnaise"],
+        sandwichTopping : ["Chicken","Pickles","Onion","Lettuce","Bacon"],
+        sandwichCost : 77,
+        sandwichCalories: 929
+    },
+    {
+        sandwichName : "Chicken Mayo",
+        sandwichBread : "Ciabatta",
+        sandwichSauce : ["Mayonnaise"],
+        sandwichTopping : ["Chicken","Pickles","Onion","Lettuce","Bacon"],
+        sandwichCost : 77,
+        sandwichCalories: 929
+    },
+    {
+        sandwichName : "Chicken Mayo",
+        sandwichBread : "Ciabatta",
+        sandwichSauce : ["Mayonnaise"],
+        sandwichTopping : ["Chicken","Pickles","Onion","Lettuce","Bacon"],
+        sandwichCost : 77,
+        sandwichCalories: 929
+    },
+
+]
--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
 displaySOTM = () =>{
-    
+    let SOTM = document.getElementById("carouselDisp");
+
+    for(let i =0; i < sandwichData.length; i++){
+        let name = sandwichData[i].sandwichName;
+        let bread = sandwichData[i].sandwichBread;
+        let sauces = sandwichData[i].sandwichSauce;
+        let toppings = sandwichData[i].sandwichTopping;
+        let cost = sandwichData[i].sandwichCost;
+        let calories = sandwichData[i].sandwichCalories;
+
+        if(i === 0){
+            SOTM.innerHTML += `
+            <div class="carousel-item active">
+                <img class="d-block w-50" src="assets/jumbo/sandwhich header.png" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>${name}</h5>
+                <p>${bread}, ${toppings.join(", ")}, ${sauces}</p>
+                <p>R${cost}.00</p>
+                <p>Calories: ${calories}</p>
+                </div>
+            </div>
+            `
+        }
+        else{
+            SOTM.innerHTML += `
+            <div class="carousel-item">
+                <img class="d-block w-50" src="assets/jumbo/sandwhich header.png" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>${name}</h5>
+                <p>${bread}, ${toppings.join(", ")}, ${sauces}</p>
+                <p>R${cost}.00</p>
+                <p>Calories: ${calories}</p>
+                </div>
+            </div>
+            `
+        }
+        
+
+    }
 }
